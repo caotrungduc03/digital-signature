a3d210335d66f678eb8f775ba2ddeffac5d6bb84
caotrungduc <trungdc2003@gmail.com>
2024-02-15 22:04:32 +0700

    init project

---
+++ asset/js/algorithm.js
@@ -0,0 +1,61 @@
+class Algorithm {
+  constructor(p, q) {
+    this.p = p;
+    this.q = q;
+    this.n = null;
+    this.d = null;
+    this.e = null;
+  }
+
+  getP() {
+    return this.p;
+  }
+
+  setP(p) {
+    this.p = p;
+  }
+
+  getQ() {
+    return this.q;
+  }
+
+  setQ(q) {
+    this.q = q;
+  }
+
+  getN() {
+    return this.n;
+  }
+
+  setN(n) {
+    this.n = n;
+  }
+
+  getD() {
+    return this.d;
+  }
+
+  setD(d) {
+    this.d = d;
+  }
+
+  getE() {
+    return this.e;
+  }
+
+  setE(e) {
+    this.e = e;
+  }
+
+  isPrime() {}
+
+  mul_inv() {}
+
+  pow() {}
+
+  generateKeys() {}
+
+  encrypt() {}
+
+  decrypt() {}
+}
